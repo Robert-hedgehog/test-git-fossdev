cd93366708298595a80b5038f642372d8af2602b
Robert <robert@MacBook-Pro-Robert.local>
2026-02-05 14:57:53 +0800

    Restricted devision of strings

--- script.py
+++ script.py
@@ -4,5 +4,7 @@ def sum(a, b):
 def divide(a, b):
 	if b == 0:
 		raise ValueError("Denominator could not be zero")
+	if isinstance(a, str) or isinstance(b, str):
+		raise ValueError("Could not devide strings")
 	return a / b
 
