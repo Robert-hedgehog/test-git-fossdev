1198ff06a30f789713e3326b136d8fed9316466b
Robert <robert@MacBook-Pro-Robert.local>
2026-03-05 13:04:12 +0800

    Update math_demo and tests

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,10 +1,30 @@
 # TODO make if with pip install -e .
 
-from math_demo import add
+# Раннее тестирование позволяет сэкономить время позднее
+# Тесты показывают наличие ошибок, а не отсутсвие
+# Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать новые ошибки (pescicide paradox)
+# Тесты покрывают как успешные, так и ошибочные кейсы
+
+from math_demo import (add, add_with_bug)
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(7, 6) == 13
     print("Test ADDITION PASSED")
 
+def test_addition_with_bug():
+    # Тесты показывают наличие ошибок, а не их отсутствие
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print("Test ADDITION PASSED")
+
+    # finally we found data that make test reliable
+    # assert add_with_bug(7, 6) == 13 # will fail here
+    
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
