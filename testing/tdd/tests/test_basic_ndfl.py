89c008782976a1b6b68dfbb7d905c6dd9ee81062
Robert <robert@MacBook-Pro-Robert.local>
2026-03-10 14:43:16 +0800

    correction of typographical errors

--- testing/tdd/tests/test_basic_ndfl.py
+++ testing/tdd/tests/test_basic_ndfl.py
@@ -6,10 +6,10 @@ def test_ndfl_tier_1_basic():
     assert calculate_ndfl(2_000_000) == 260_000
 
 def test_ndfl_tier_2_basic():
-    assert calculate_ndfl(4_000_000) == 260_000
+    assert calculate_ndfl(4_000_000) == 552_000
 
 def test_ndfl_tier_3_basic():
-    assert calculate_ndfl(10_000_000) == 552_000
+    assert calculate_ndfl(10_000_000) == 1_602_000
 
 def test_ndfl_tier_4_basic():
     assert calculate_ndfl(30_000_000) == 5_402_000
