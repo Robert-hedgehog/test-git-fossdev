dbe865299b9412680cdbb53a5e1a4993b3dc3c9c
Robert <robert@MacBook-Pro-Robert.local>
2026-04-02 15:09:37 +0800

    Added support for docstring

--- docs_domain/docs/source/conf.py
+++ docs_domain/docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "src"))
+
 project = 'sales'
 copyright = '2026, Robert-hedgehog'
 author = 'Robert-hedgehog'
@@ -14,7 +19,20 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode"
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown"
+}
+
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = []
